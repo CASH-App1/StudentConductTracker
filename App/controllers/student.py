9e21e21a3b30fb9566533089a092128fe9c94f31
alliyah01 <124291804+alliyah01@users.noreply.github.com>
2023-10-09 15:11:14 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -18,9 +18,17 @@ def get_all_students():
     student_data = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName} for student in students]
     return student_data
 
+# Controller to get a list of students with a specific first name and last name
+def get_students_by_name(first_name, last_name):
+    students = Student.query.filter_by(firstName=first_name, lastName=last_name).all()
+    
+    # Create a list of dictionaries containing student data
+    student_search = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName} for student in students]
+    return student_search
+    
 # Controller to update a student
 def update_student(studentID, new_first_name, new_last_name):
-    student = Student.query.filter_by(studentID=studentID).first()
+    student = Student.query.get(studentID)
     if student:
         student.firstName = new_first_name
         student.lastName = new_last_name
