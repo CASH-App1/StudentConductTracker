f63cb9ea87b54eb0791bdfd39d2d0512e022ffa4
SGajadhar <97929776+SGajadhar@users.noreply.github.com>
2023-10-10 15:54:38 -0400

    3 functions were edited

--- App/controllers/review.py
+++ App/controllers/review.py
@@ -1,35 +1,47 @@
 
-from App.model import *
+from App.models import *
+from App.database import db 
 
 #Controller of staff to log review for student
-def log_review(studentID, review, type):
-    newReview = Review(studentID=studentID, review=review, type=type)
-    db.session.add(newReview)
-    db.session.commit()
+def log_review(staffID, studentID, review, type):
+    student = Student.query.filter_by(studentID=studentID).first()
+    if not student:
+        return None
+    
+    newReview = Review(staffID=staffID, studentID=studentID, review=review, type=type)
+    if newReview:
+        student.reviews.append(newReview)
+        db.session.add(newReview)
+        db.session.commit()
+        return newReview
+    return None
+    
 
 #Retrieving reviews done by staff
 def get_all_reviews():
     return Review.query.all()
 
 #Upvote review
-def upvote_review(reviewID):
-    review = Review.query.get(reviewID)
-    if review:
-        upvote = review.upvoteReview(reviewID)
+def upvote_review(staffID, reviewID):
+    staff = Staff.query.get(staffID)
+    
+    if staff:
+        upvote = staff.upvoteReview(reviewID)
         db.session.add(upvote)
         db.session.commit()
-        return True
-    return False
+        return upvote
+    return None
 
 #Downvote review
-def downVote_review(reviewID):
-    review = Review.query.get(reviewID)
-    if review:
-        downvote = review.downvoteReview(reviewID)
+def downVote_review(staffID, reviewID):
+    staff = Staff.query.get(staffID)
+    
+    if staff:
+        downvote = staff.downvoteReview(reviewID)
         db.session.add(downvote)
         db.session.commit()
-        return True
-    return False
+        return downvote
+    return None
 
 #Controller to get a review by reviewID
 def get_review(reviewID):
