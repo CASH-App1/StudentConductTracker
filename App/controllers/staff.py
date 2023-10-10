6006bc3704f25cd6c78fbd5088202c93c6c7e1aa
haileyfritz <haileyfritz24@gmail.com>
2023-10-10 15:23:34 +0000

    made fixes to staff controllers

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -3,22 +3,22 @@ from App.database import db
 
 # Controller to create a staff member
 def createStaff(username, password, email, firstName, lastName):
-    newStaff = Staff(username=username, password=password, email=email, firstName=firstName, lastName=lastName)
+    newStaff = StaffMember(username=username, password=password, email=email, firstName=firstName, lastName=lastName)
     db.session.add(newStaff)
     db.session.commit()
     return newStaff
     
 # Controller to get a staff member by username
 def get_staff_by_username(username):
-    return Staff.query.filter_by(username=username).first()
+    return StaffMember.query.filter_by(username=username).first()
 
 # Controller to get a staff member by ID
 def get_staff(staffID):
-    return Staff.query.get(staffID)
+    return StaffMember.query.get(staffID)
 
 # Controller to get a list of all staff members
 def get_all_staff():
-    return Staff.query.all()
+    return StaffMember.query.all()
 
 # Controller to update a staff member's username
 def update_staff(staffID, new_username, new_email):
@@ -33,9 +33,9 @@ def update_staff(staffID, new_username, new_email):
 
 # Controller to change staff password
 def update_password(staff_id, new_password):
-    staff = Staff.query.filter_by(staffID=staff_id).first()
+    staff = StaffMember.query.get(staff_id)
     if staff:
-        staff.password = new_password
+        staff.password = set_password(new_password)
         db.session.add(staff)
         db.session.commit()
         return staff
@@ -43,7 +43,7 @@ def update_password(staff_id, new_password):
 
 
 def get_all_staff_json():
-    staff = Staff.query.all()
+    staff = StaffMember.query.all()
     if not staff:
         return []
     staff = [s.get_json() for s in staff]
