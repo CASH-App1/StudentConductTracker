fd6becf9c2686fbe8e4cad6a9f96fc7768978ad8
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-10-10 00:35:23 -0400

    Update student.py

--- App/models/student.py
+++ App/models/student.py
@@ -1,6 +1,17 @@
 from App.database import db
 
 class Student(db.Model):
-  studentID: db.Column(db.String(5), nullable=False)
+  studentID: db.Column(db.String(5), primary_key= True)
   fname: db.Column(db.String(30), nullable=False)
+  lname: db.Column(db.String(40), nullable=False)
+  karmaScore: db.Column(db.Integer, nullable=False)
+  reviews: db.relationship('Listing',backref=db.student('user', lazy='joined'))
+
+def updateKarmaScore( self, karmaScore):
+     self.karmaScore += karmaScore
+      
+      
+      
+  
+  
   
