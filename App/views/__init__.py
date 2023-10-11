9c7807c8404324370bc93ec7f6435905eb542ac4
SGajadhar <97929776+SGajadhar@users.noreply.github.com>
2023-10-11 11:17:12 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -3,6 +3,7 @@
 from .staff import user_views
 from .auth import auth_views
 from .review import review_views
+from .index import index_views
 
-views = [user_views, auth_views, review_views] 
+views = [user_views, auth_views, review_views, index_views] 
 # blueprints must be added to this list
