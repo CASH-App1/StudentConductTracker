1108e5b2724d9ca514f7bb68232be0442c9e1fb4
alliyah01 <124291804+alliyah01@users.noreply.github.com>
2023-10-11 00:08:29 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,32 +1,32 @@
-from werkzeug.security import check_password_hash, generate_password_hash
+ffrom werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin, login_user
 from App.database import db
 import random
 
 class StaffMember(db.Model) :
     staffID = db.Column(db.Integer, primary_key=True)
-    fName = db.Column(db.String(30), nullable=False)
-    lName = db.Column(db.String(40), nullable=False)
+    firstName = db.Column(db.String(30), nullable=False)
+    lastName = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(30), nullable=False)
-    reviews = db.relationship('Review', backref= db.backref('staff', lazzy='joined'))
+    reviews = db.relationship('Review', backref= db.backref('staff', lazy='joined'))
 
     def __init__(self, username, password, email, firstName, lastName) :
         self.username = username
         self.set_password(password)
         self.email = email
-        self.firstName = fName
-        self.lastName = lName
+        self.firstName = firstName
+        self.lastName = lastName
 
     def  __repr__(self):
-        return f'<StaffMember {self.staffID} - {self.username} - {self.fName} - {self.lName} - {self.email}>'
+        return f'<StaffMember {self.staffID} - {self.username} - {self.firstName} - {self.lastName} - {self.email}>'
 
     def toDict(self):
         return{
             'Staff ID': self.staffID,
-            'First Name': self.fName,
-            'Last Name': self.lName,
+            'First Name': self.firstName,
+            'Last Name': self.lastName,
             'Email': self.email,
             'Username': self.username      
         }
