a891b9d5d45fc025671bc6bd5f155d6c22135580
haileyfritz <haileyfritz24@gmail.com>
2023-10-23 15:47:32 +0000

    student & staff unit tests

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,15 +3,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User
-from App.controllers import (
-    create_user,
-    get_all_users_json,
-    login,
-    get_user,
-    get_user_by_username,
-    update_user
-)
+from App.models import *
+from App.controllers import *
 
 
 LOGGER = logging.getLogger(__name__)
@@ -19,28 +12,42 @@ LOGGER = logging.getLogger(__name__)
 '''
    Unit Tests
 '''
-class UserUnitTests(unittest.TestCase):
+class StudentUnitTests(unittest.TestCase):
 
-    def test_new_user(self):
-        user = User("bob", "bobpass")
-        assert user.username == "bob"
+    def test_new_student(self):
+        newStudent = Student("816011111", "Dale", "Barbara")
+        assert (newStudent.username, newStudent.fname, newStudent.lname) == ("816011111", "Dale", "Barbara")
 
-    # pure function no side effects or integrations called
-    def test_get_json(self):
-        user = User("bob", "bobpass")
-        user_json = user.get_json()
-        self.assertDictEqual(user_json, {"id":None, "username":"bob"})
+    def test_student_toDict(self):
+        newStudent = Student("816011111", "Dale", "Barbara")
+        student_json = newStudent.toDict()
+        self.assertDictEqual(student_json, {"Student ID":"816011111", "First Name":"Dale", "Last Name":"Barbara", "Karma Score":"0"})
     
+    def updateKarmaScore(self):
+        newStudent = Student("816011111", "Dale", "Barbara")
+        newStaff = Staff("RobertJackson", "robby123", "robertjackson@mail.com", "Robert", "Jackson")
+        newReview = newStaff.createReview("816011111", "This student continues to show great potential", "positive")
+        newReview.upvote()
+        newStudent.updateKarmaScore()
+        assert(newStudent.karmaScore == 1)
+
+class StaffUnitTests(unittest.TestCase):
+
     def test_hashed_password(self):
         password = "mypass"
         hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password)
-        assert user.password != password
+        newStaff = Staff("bob", "bobpass", "bob@mail.com", "Bobby", "Smith")
+        assert newStaff.password != password
 
     def test_check_password(self):
         password = "mypass"
-        user = User("bob", password)
-        assert user.check_password(password)
+        newStaff = Staff("bob", password, "bob@mail.com", "Bobby", "Smith")
+        assert newStaff.check_password(password) 
+
+    def test_create_review(self):
+        newStaff = Staff("bob", password, "bob@mail.com", "Bobby", "Smith")
+        newReview = newStaff.createReview("816011111", "This student continues to show great potential", "positive")
+        assert (newReview.studentID, newReview.description, newReview.reviewType) == ("816011111", "This student continues to show great potential", "positive")
 
 '''
     Integration Tests
