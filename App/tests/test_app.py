857c5fcd0cc4f22991c8d7ac077c5b5440205d9b
alliyah01 Ali <alliyah.hosein@gmail.com>
2023-10-24 16:20:55 +0000

    updated import statements

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,15 +3,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User
-from App.controllers import (
-    create_user,
-    get_all_users_json,
-    login,
-    get_user,
-    get_user_by_username,
-    update_user
-)
+from App.models import Review, StaffMember, Student 
+from App.controllers import *
 
 
 LOGGER = logging.getLogger(__name__)
