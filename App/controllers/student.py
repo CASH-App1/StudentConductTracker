c789d8b9fe2449c624182250118b1e05a68203dc
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-12-01 19:51:37 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -2,20 +2,20 @@ from App.models import Student
 from App.database import db
 
 # Controller to add a new student
-def add_student(studentID, first_name, last_name):
-    new_student = Student(studentID=studentID, firstName=first_name, lastName=last_name)
+def add_student(studentID, first_name, last_name, email):
+    new_student = Student(, studentID=studentID, firstName=first_name, lastName=last_name, email=email)
     db.session.add(new_student)
     db.session.commit()
     return new_student
 
 # Controller to get a student by username (student_id)
 def get_student(studentID):
-    return Student.query.filter_by(studentID=student_id).first()
+    return Student.query.filter_by(studentID=student_studentID).first()
 
 # Controller to get a list of all students
 def get_all_students():
     students = Student.query.all()
-    student_data = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName} for student in students]
+    student_data = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName, 'email': student.email} for student in students]
     return student_data
 
 # Controller to get a list of students with a specific first name and last name
@@ -23,15 +23,16 @@ def get_students_by_name(first_name, last_name):
     students = Student.query.filter_by(firstName=first_name, lastName=last_name).all()
     
     # Create a list of dictionaries containing student data
-    student_search = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName} for student in students]
+    student_search = [{'studentID': student.studentID, 'firstName': student.firstName, 'lastName': student.lastName, 'email':student.email} for student in students]
     return student_search
     
 # Controller to update a student
-def update_student(studentID, new_first_name, new_last_name):
+def update_student(studentID, new_first_name, new_last_name, new_email):
     student = Student.query.get(studentID)
     if student:
         student.firstName = new_first_name
         student.lastName = new_last_name
+        student.email = new_email
         db.session.add(student)
         db.session.commit()
         return student
